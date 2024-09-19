b7368a2715f88e58ef262b77805e1395fe2b1a3c
Kqrma0124 <daiki81388138@gmail.com>
2024-09-19 05:18:27 +0000

    9/19 Java scriptの導入

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector("h1").textContent="松本"
+document.querySelector("h2").textContent="松本"
+
+//document　とは
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす*/
+
+//DOM
+/*ドキュメントオブジェクトモデル(Document Object Model)の略でWebページの要素や
+コンテンツなどをツリー構造で表現したデータモデル*/
+
+//上記コードの意味
+//documednt.querySelector("h1").textContent="松本"
+
+//ドキュメントの中の("h1")タグを探して
+//documednt.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent="松本"
+
