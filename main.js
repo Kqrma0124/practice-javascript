c6c6d7b0c77c515b0e139be6f282ea6eda2ebd64
Kqrma0124 <daiki81388138@gmail.com>
2024-10-18 06:18:35 +0000

    10_18　ゲーム作り

--- main.js
+++ main.js
@@ -1,34 +1,99 @@
+const gameSelecter = document.getElementById('game-selector');
+const gameContainer = document.getElementById('game-container');
 
-    function errorInput(){
+gameSelecter.addEventListener("change",function(){
 
-    const month =document.getElementById("month").value;
-    const day = document.getElementById("day").value;
+    gameContainer.innerHTML = "";
 
-    if(month<1 || month>12){
+    switch(gameSelecter.value){
+        case"none":
+        gameContainer.textContent="ゲームを選ぶことここに表示されます"
+            break;
 
-    alert("適切な値ではありません");
-    return
+        case"clik-counter":
+           ClickCounterGame();
+            break;
 
+        case"namber-guess":
+        NumberGuessGame();
+            break;
     }
+})
 
-   
-    console.log("month:",month,"day:",day)
+function NumberGuessGame(){
+    const randoumNumber = Math.floor(Math.randoum()*100)+1;
+
+    let message = document.createElement("p");
+
+let input = document.createElement("input");
+nput.type="Number";
+input.max=100;
+input.min=1;
+input.placeholder="好きな数字を入れてください(1~100)"
+
+button.textContent = "予想"
+let button = document.createElement("button");
+
+
+
+button.textContent = "予想"
+
+button.addEventListener("click",function(){
+    const userGuess = parseInt(input.value);
+    if(userGuess === randoumNumber){
+        message.textContent="正解!"
+    }else if(userGuess > randomNumber){
+        message.textContent = "値が高い！"
+    }else {
+        message.textContent = "値が小さい！"
+    }
+})
+
+
+
+message.textContent = "文字"
+
+    gameContainer.appendChild(input);
+    gameContainer.appendChild(button);
+    gameContainer.appendChild(message);
 
 }
 
-    const = monthSlect = document.getElementById("month2");
+function ClickCounterGame(){
 
-    monthSelectlist()
+    let count = 0 ;
+   
+    
+    let counter = document.createElement("p");
 
-    
-    function monthSelectlist(){
-    
-    for(let i=1; i<=12;i++) {
-    
-    let option =document.createElement('option');
-    option.value = i;
-    option.textContent = i + 月;
-    monthSlect.appendChild(option);
-    }
+    let button1 = document.createElement("button");
+    button1.textContent = "+1ボタン";
+
+
+ let button2 = document.createElement("button");
+    button2.textContent = "+10ボタン";
+  
+    let button3 = document.createElement("button");
+    button3.textContent = "+100ボタン";
+   
+
+    button1.addEventListener("click",function(){
+        count++;
+        counter.textContent = count;
+    })
+
+    button2.addEventListener("click",function(){
+        count = count +10;
+        counter.textContent = count;
+    })
+
+    button3.addEventListener("click",function(){
+        count = count +100;
+        counter.textContent = count;
+    })
+    gameContainer.appendChild(button1);  
+    gameContainer.appendChild(button2);    
+    gameContainer.appendChild(button3);  
+    gameContainer.appendChild(counter);
 }
 
