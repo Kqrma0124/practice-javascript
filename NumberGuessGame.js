ab2b246ea0aa55c6c94c03443c4078490f5031b2
Kqrma0124 <daiki81388138@gmail.com>
2024-11-21 06:48:51 +0000

    数当て　修正

--- NumberGuessGame.js
+++ NumberGuessGame.js
@@ -1,38 +1,28 @@
 function NumberGuessGame(){
-    const randoumNumber = Math.floor(Math.randoum()*100)+1;
-
+    const randomNumber = Math.floor(Math.random()*100)+1;
     let message = document.createElement("p");
 
-let input = document.createElement("input");
-input.type="Number";
-input.max=100;
-input.min=1;
-input.placeholder="好きな数字を入れてください(1~100)"
+    let input = document.createElement("input");
+    input.type = "number";
+    input.max = 100;
+    input.min = 1;
+    input.placeholder = "好きな数字を入力してください（1-100）"
 
+    let button = document.createElement("button");
+    button.textContent = "予想";
+    button.addEventListener("click",function(){
+        const userGuss = parseInt(input.value);
+        if(userGuss === randomNumber){
+            message.textContent = "正解！"
+        }else if(userGuss > randomNumber){
+            message.textContent = "値が高いよ！"
+        }else{
+            message.textContent = "値が小さいよ！"
+        }
+    })
 
-let button = document.createElement("button");
-
-
-
-button.textContent = "予想"
-
-button.addEventListener("click",function(){
-    const userGuess = parseInt(input.value);
-    if(userGuess === randoumNumber){
-        message.textContent="正解!"
-    }else if(userGuess > randomNumber){
-        message.textContent = "値が高い！"
-    }else {
-        message.textContent = "値が小さい！"
-    }
-})
-
-
-
-message.textContent = "文字"
 
     gameContainer.appendChild(input);
     gameContainer.appendChild(button);
     gameContainer.appendChild(message);
-
 }
