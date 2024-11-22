b2209cff0b910b55cdb8ad1e2ff6d56d280122a2
Kqrma0124 <daiki81388138@gmail.com>
2024-11-22 06:14:36 +0000

    11_22

--- RPS.js
+++ RPS.js
@@ -1,6 +1,9 @@
 function RPS(){
 
-    const choices = ["gu","choki","pa","muteki"];
+    const choices = ["gu","choki","pa"];
+    
+    let handImage = ""
+
     /*この書き方は、配列を意味する　C言語参照*/ 
     
     // let select = document.createElement("select")
@@ -23,24 +26,85 @@ function RPS(){
     // select.appendChild(select);
 
     let select = document.createElement("select")
+    select.id = "choice"
+
     choices.forEach(choice => {
     let option =document.createElement("option")
     option.value =choice
     option.textContent = choice;
+    option.id = choice;
     select.appendChild(option);
+   
+
 
     })
 
-    gameContainer.appendChild(select);
+    let choiceButton = document.createElement("button")
+    choiceButton.textContent="決定"
+
+    gameContainer.appendChild(choiceButton)
+
+
+    choiceButton.addEventListener("click",function(){
+        switch(select.value){
+                case "gu":
+                    image.src="https://tsukatte.com/wp-content/uploads/2019/03/janken_gu-940x940.png";
+                    break;
+            
+                case "choki":
+                    image.src="https://tsukatte.com/wp-content/uploads/2019/03/janken_choki.png";
+                    break;
+            
+                case "pa":
+                    image.src="https://tsukatte.com/wp-content/uploads/2019/03/janken_pa-940x940.png";
+                    break;
+            
+                default:
+                    break;
+            
+                }
+                
+                gameContainer.appendChild(image);
+                gameContainer.appendChild(enemyImage);
+
+                let enemyHand = choices[Math.floor(Math.random()*choices.length)];
+                
+                switch(enemyHand){
+                    case "gu":
+                        enemyImage.src="https://tsukatte.com/wp-content/uploads/2019/03/janken_gu-940x940.png";
+                        break;
+                
+                    case "choki":
+                        enemyImage.src="https://tsukatte.com/wp-content/uploads/2019/03/janken_choki.png";
+                        break;
+                
+                    case "pa":
+                        enemyImage.src="https://tsukatte.com/wp-content/uploads/2019/03/janken_pa-940x940.png";
+                        break;
+                
+                    default:
+                        break;
+                
+                    }
+                
+    })
+
+    
+
 
     let image =document.createElement("img")
     image.alt="Choice"
     image.style.width = "100px"
     image.style.height = "100px"
-    image.src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiusq_ptNOOC9XkVvWTa88nhB6I7n12fsf95zdnS-n269HpN9dVRd0JCCV0iL2u_tWrDU5XySM8-i9u38-tXp0Wgu6qRF4p-5A1djjVskwkB0SQFxULDss8Uj1o7CYfbMNRpT-kfn3cG4E/s800/janken_gu.png";
+    
+    let enemyImage =document.createElement("img")
+    enemyImage.alt="Choice"
+    enemyImage.style.width = "100px"
+    enemyImage.style.height = "100px"
 
-    gameContainer.appendChild(image)
+    gameContainer.appendChild(select);
 
+    
     /*forEach は配列やオブジェクトなど複数のデータがあるものに使える。全部やる関数
     配列 forEach(配列の中身 =>{命令}) */
 
