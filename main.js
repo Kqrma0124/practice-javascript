6906daa66d73aaf16f7fa8cdddcc6176d5907177
Kqrma0124 <daiki81388138@gmail.com>
2025-01-24 06:17:53 +0000

    判定ボタンの追加

--- main.js
+++ main.js
@@ -1,66 +1,67 @@
-function ClickCounterGame(){
-    let count = 0;
-    const gameContainer=document.getElementById("game-container");
+// function ClickCounterGame(){
+//     let count = 0;
+//     const gameContainer=document.getElementById("game-container");
 
 
-let button1 = document.createElement("button");
-button1.textContent="+1ボタン" 
+// let button1 = document.createElement("button");
+// button1.textContent="+1ボタン" 
 
-let button2 = document.createElement("button")
-button2.textContent="10ボタン"
+// let button2 = document.createElement("button")
+// button2.textContent="10ボタン"
 
-let button3 = document.createElement("button")
-button3.textContent="リセット"
+// let button3 = document.createElement("button")
+// button3.textContent="リセット"
 
-let counter = document.createElement("p");
-counter.textContent = count;
+// let counter = document.createElement("p");
+// counter.textContent = count;
 
-button1.addEventListener("click",function(){
+// button1.addEventListener("click",function(){
 
-    if(count<100){
+//     if(count<100){
 
-    count++;
+//     count++;
    
-}
+// }
    
-    counter.textContent=count;
+//     counter.textContent=count;
 
-})
+// })
 
-button2.addEventListener("click",function(){
+// button2.addEventListener("click",function(){
 
-    if(count<=90){
+//     if(count<=90){
     
-        count = count + 10;
-    }
-        counter.textContent=count;
+//         count = count + 10;
+//     }
+//         counter.textContent=count;
     
-})
+// })
 
-button3.addEventListener("click",function(){
+// button3.addEventListener("click",function(){
 
 
-  count = 0;
-  counter.textContent=count;
+//   count = 0;
+//   counter.textContent=count;
 
-})
+// })
 
-gameContainer.appendChild(button1);
-gameContainer.appendChild(button2);
-gameContainer.appendChild(button3);
-gameContainer.appendChild(counter);
+// gameContainer.appendChild(button1);
+// gameContainer.appendChild(button2);
+// gameContainer.appendChild(button3);
+// gameContainer.appendChild(counter);
 
-}
+// }
 
-ClickCounterGame();
+// ClickCounterGame();
 
 function NUmberGuessGame(){
     const gameContainer = document.getElementById("game-container");
 
     const ramdomNumber = Math.floor(Math.random()*100)+1;
-    
+
     let message =document.createElement("p");
-    message.textContent = ramdomNumber
+
+    
     
     let input = document.createElement("input");
     
@@ -71,6 +72,32 @@ function NUmberGuessGame(){
     gameContainer.appendChild(message);
     gameContainer.appendChild(input);
 
+
+    let button = document.createElement("button")
+    button.textContent="確認"
+
+    button.addEventListener("click",function(){
+
+        const val= parseInt(input.value);
+
+        if(ramdomNumber == val){
+            message.textContent="正解"
+        }
+
+        else  if(ramdomNumber<val){
+        message.textContent="大きい"
+        }
+        
+        else{
+            message.textContent="小さい"
+
+        }
+
+    })
+    gameContainer.appendChild(button);
+
 }
+    
+
 
 NUmberGuessGame()
